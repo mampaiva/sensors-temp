07ee79cb6d4bfb79539dcf8622180727fd7f72b0
Mateus Paiva <64433509+mampaiva@users.noreply.github.com>
2020-10-30 18:58:11 -0300

    Add files via upload

--- graph-temp.sh
+++ graph-temp.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-rm sensors-data.dat
 
 echo "último registro:" 
 tail -n 1 temp.dat
+echo "In the last 24h the maximum temperature was:" 
+sort -n temp.dat | tail -n 1 | echo celsius degree
 
-grep "Package id 0:  " temp.dat >> temp2.dat
-nl  -s '     ' temp2.dat >> sensors-data.dat
+nl  -s '     ' temp.dat > sensors-data.dat
 
-rm temp2.dat
 echo "set term postscript color enhanced \"Helvetica\" 20
 
 set pointsize 1.4 
@@ -26,7 +25,7 @@ set key at graph 0.95, graph 0.45 vertical maxrows 7
 set xlabel \"x10 seconds\" 
 
 set label 1  \"\" at graph 0.16, graph 0.85
-plot   \"sensors-data.dat\" index 0 u 1:5 w l lw 4 lc rgb \"black\" ti \"\" " >> sensors.gpl
+plot   \"sensors-data.dat\" index 0 u 1:2 w l lw 4 lc rgb \"black\" ti \"\" " >> sensors.gpl
 
 gnuplot sensors.gpl &
 atril sensors.ps &
--- temp.sh
+++ temp.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-rm temp.dat
+
+    echo "temperature in celsius degree with the follow date and hour of the record:" > temp.dat
 
 while :
 do
-    sensors | grep "Package id 0:  " >> temp.dat
+    sensors | grep "Package id 0:  " | awk -F '[+]' '{print $2}' | awk -F '[.]' '{print $1}' >> temp.dat
     date >> temp.dat
 	sleep 10
+    a=`wc -l temp.dat | awk -F '[ ]' '{print $1}'`
+    if [ $a -gt 17280 ]
+    then
+        echo "In the last 24h the maximum temperature was:" >> temp.dat
+        sort -n temp.dat | tail -n 1 >> temp.dat
+        tail -n 26 temp.dat > temp.dat.tmp
+        rm temp.dat
+        cat temp.dat.tmp > temp.dat
+        rm temp.dat.tmp
+    fi    
 done
